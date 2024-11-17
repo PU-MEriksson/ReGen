b0556e04a19a8d296ac5c340a9585b60f1a24b03
Johan-Hagman <steven.gerard.hagman@gmail.com>
2024-11-17 21:47:04 +0100

    test

---
+++ header.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/css/header.css">
+    <title>ReGen</title>
+</head>
+
+<body>
+    <div class="discount">
+        <div class="text">
+            Bli medlem och få 15% rabatt
+        </div>
+    </div>
+</body>
+
+</html>
