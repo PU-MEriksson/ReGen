fc2c8f097a1245bbc8dfa6f4c83df9cf659ad279
Johan-Hagman <steven.gerard.hagman@gmail.com>
2024-11-20 17:15:37 +0100

    worked on the helmet section for mobile and adjusted some css on the dropdown menu in header

--- main.php
+++ main.php
@@ -7,8 +7,14 @@
         </div>
 
         <div class="helmet-content">
-            <div class="text-container"></div>
-            <button>Utforska hjälm</button>
+            <div class="text-container">
+                <div class="header-1">VERTEX</div>
+                <div class="header-2">SKIDHJÄLM</div>
+                <div class="header-3">Det självklara valet för dig som vill ha både prestanda och stil i backen.</div>
+            </div>
+            <div class="helmet-button">
+                <button class="helmet-button-text">Utforska hjälm</button>
+            </div>
         </div>
     </div>
     <script src="/navbar.js">
